f807a6a0a21d1ddad6063031d73c6d92a3140f73
Karl Nicholas <karl.nicholas@outlook.com>
2022-12-06 15:15:13 -0500

    updates

--- EssentialMonolithUI/src/Analysis.js
+++ EssentialMonolithUI/src/Analysis.js
@@ -1,21 +1,118 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import http from "./http-common";
 
-const AnalysisPage = () => {
+function AnalysisPage() {
   const [analysisRun, setAnalysisRun] = useState(null);
   const [dimensions, setDimensions] = useState([]);
   const [olapResult, setOlapResult] = useState(null);
   const [whereSelects, setWhereSelects] = useState([]);
   const [factCount, setFactCount] = useState(0);
+  const dropdownTitles = useRef([]);
+  const dropdownIds = useRef([]);
 
   useEffect(() => {
     http.get('/analysis').then(response => {
       setDimensions(response.data.analysisDimensions);
       setAnalysisRun(response.data.analysisRun);
       setFactCount(response.data.factCount);
-    });
+      updateDimensions(response.data.analysisDimensions);
+      });
   }, []);
 
+  const updateDimensions = (dimensions) => {
+    dropdownTitles.current = new Array(dimensions.length).fill("");
+    dropdownIds.current = new Array(dimensions.length).fill(null);
+  };
+
+
+  const showAnalysisRun = () => {
+    if (analysisRun != null) {
+      return (
+        <tr><td>{analysisRun.populating ? <button className="btn btn-primary" onClick={() => handleRefreshClick(setDimensions, setAnalysisRun, setFactCount)}>Refresh</button> : <button className='btn btn-primary' onClick={() => handlePopulateClick(setAnalysisRun)}>Populate</button>}</td><td>{factCount}</td><td>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(analysisRun.lastRunDateTime)}</td><td>{String(analysisRun.populating)}</td></tr>
+      );
+    } else {
+      return (
+        <tr><td><button className="btn btn-primary" onClick={() => handlePopulateClick(setAnalysisRun)}>Populate</button></td><td>No Count</td><td>Not Run</td><td>FALSE</td></tr>
+      );
+    }
+  }
+  const tableHeaders = () => {
+    if (dimensions.length > 0) {
+      return (
+        dimensions.map(dimension => <th key={dimension.name}>{dimension.name}</th>)
+      );
+    }
+  };
+  const tableBody = () => {
+    if (dimensions.length > 0) {
+      return (dimensions.map((dlist, index) =>
+        <td key={dlist.name}>
+          <div key={index} className="dropdown">
+            <button className='btn btn-primary dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false" id={dlist.name}>{dimensions[index].title !== undefined ? dimensions[index].title : ''}</button >
+            <ul className="dropdown-menu">
+              <li key={index}><button className='dropdown-item' type="button" onClick={() => onTargetSelect("", null, index)}>&nbsp;</button></li>
+              {dlist.dimensions.map((dvalue, idx) =>
+                <li key={idx}><button className='dropdown-item' type="button" onClick={() => onTargetSelect(dvalue.name, dvalue.id, index)}>{dvalue.name}</button></li>
+              )}
+            </ul>
+          </div>
+        </td>)
+      );
+    }
+  }
+  const card = () => {
+    if (dimensions.length > 0) {
+      return (dimensions.map((dlist, index) =>
+        <div key={index} className="dropdown">
+          <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" key={dlist.name} id={dlist.name}>{dlist.name}</button >
+          <ul className="dropdown-menu">
+            {dlist.dimensions.map((dvalue, index) =>
+              <li key={index}><button className="dropdown-item" type='button' onClick={() => onWhereSelect(dimensions[index].name, dvalue.name, dvalue.id, whereSelects, setWhereSelects)}>{dvalue.name}</button></li>
+            )}
+          </ul>
+        </div>
+      ));
+    }
+  }
+  const handlePopulateClick = () => {
+    http.get('/analysis/populate').then(response => {
+      setAnalysisRun(response.data);
+    });
+  }
+  const handleRefreshClick = () => {
+    http.get('/analysis').then(response => {
+      setDimensions(response.data.analysisDimensions);
+      setAnalysisRun(response.data.analysisRun);
+      setFactCount(response.data.factCount);
+      updateDimensions(response.data.analysisDimensions);
+    });
+  }
+  const onTargetSelect = (name, id, index) => {
+    let titles = dropdownTitles.current.slice(); //creates the clone of the state
+    titles[index] = name;
+    let ids = dropdownIds.current.slice(); //creates the clone of the state
+    ids[index] = id;
+    dropdownTitles.current = titles;
+    dropdownIds.current = ids;
+  };
+
+  const onQueryClick = () => {
+    var queryString = '';
+    var queryMark = '?';
+    var i;
+    for (i = 0; i < dropdownTitles.length; ++i) {
+      var select = dropdownIds[i];
+      if (select !== null) {
+        queryString += queryMark + dimensions[i].name + '=' + select;
+        queryMark = '&';
+      }
+    }
+    (async () => {
+      http.get('/analysis/billingresult' + queryString).then(response => {
+        setOlapResult(response.data);
+      })
+    })();
+  };
   return (
     <div>
       <table className="table">
@@ -23,37 +120,29 @@ const AnalysisPage = () => {
           <tr><th>Populate</th><th>Fact Count</th><th>Last Run Time</th><th>Populating</th></tr>
         </thead>
         <tbody>
-          {showAnalysisRun(analysisRun, factCount, setAnalysisRun, setDimensions, setFactCount)}
+          {showAnalysisRun()}
         </tbody>
       </table>
       <div>
-        {card(dimensions)}
+        {card()}
       </div>
       <table className="table">
         <thead>
-          <tr><th>Query</th>{tableHeaders(dimensions)}</tr>
+          <tr><th>Query</th>{tableHeaders()}</tr>
         </thead>
         <tbody>
-          <tr><td><button className="btn btn-primary" onClick={() => onQueryClick(dimensions, setOlapResult)}>Query</button></td>{tableBody(dimensions)}</tr>
+          <tr><td><button className="btn btn-primary" onClick={() => onQueryClick(dimensions, setOlapResult)}>Query</button></td>{tableBody()}</tr>
         </tbody>
       </table>
       {olapResultDisplay(olapResult)}
     </div>
   );
 }
-const onTargetSelect = (name, id, index) => {
-  let titles = this.state.dropdownTitles.slice(); //creates the clone of the state
-  titles[index] = name;
-  let ids = this.state.dropdownIds.slice(); //creates the clone of the state
-  ids[index] = id;
-  this.setState({ dropdownTitles: titles, dropdownIds: ids });
-};
-const onWhereSelect = (name, id, index) => {
-  let d = this.state.dimensions[index];
-  let dname = d.name;
-  let whereSelects = this.state.whereSelects.slice(); //creates the clone of the state
-  whereSelects.push({ 'display': dname + ':' + name + ":" + id, 'property': dname, 'id': id });
-  // this.setState({ whereSelects: whereSelects });
+
+function onWhereSelect(dname, name, id, whereSelects, setWhereSelects) {
+  let ws = whereSelects.slice(); //creates the clone of the state
+  ws.push({ 'display': dname + ':' + name + ":" + id, 'property': dname, 'id': id });
+  setWhereSelects(ws);
   // var whereList = [];
   // var i;
   // for (i = 0; i < this.state.whereSelects.length; ++i) {
@@ -68,92 +157,6 @@ const onWhereSelect = (name, id, index) => {
   //  })();
 };
 
-function handlePopulateClick(setAnalysisRun) {
-  http.get('/analysis/populate').then(response => {
-    setAnalysisRun(response.data);
-  });
-}
-function handleRefreshClick(setDimensions, setAnalysisRun, setFactCount) {
-  http.get('/analysis').then(response => {
-    setDimensions(response.data.analysisDimensions);
-    setAnalysisRun(response.data.analysisRun);
-    setFactCount(response.data.factCount);
-  });
-}
-function onQueryClick(dimensions, dropdownTitles, dropdownIds, setOlapResult) {
-  var queryString = '';
-  var queryMark = '?';
-  var i;
-  for (i = 0; i < dropdownTitles.length; ++i) {
-    var select = dropdownIds[i];
-    if (select !== null) {
-      queryString += queryMark + dimensions[i].name + '=' + select;
-      queryMark = '&';
-    }
-  }
-  (async () => {
-    http.get('/analysis/billingresult' + queryString).then(response => {
-      setOlapResult(response.data);
-    })
-  })();
-};
-function tableHeaders(dimensions) {
-  if (dimensions.length > 0) {
-    return (
-      dimensions.map(dimension => <th key={dimension.name}>{dimension.name}</th>)
-    );
-  }
-};
-function card(dimensions) {
-  if (dimensions.length > 0) {
-    return (dimensions.map((dlist, index) =>
-      <div key={index} className="dropdown">
-        <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" key={dlist.name} id={dlist.name}>{dlist.name}</button >
-        <ul className="dropdown-menu">
-          {dlist.dimensions.map((dvalue, index) =>
-            <li key={index}><a className="dropdown-item" href="#" onClick={() => onWhereSelect(dvalue.name, dvalue.id, index)}>{dvalue.name}</a></li>
-          )}
-        </ul>
-      </div>
-    ));
-  }
-}
-function whereGroup(whereSelects) {
-  if (whereSelects.length > 0) {
-    return (whereSelects.map((w, index) =>
-      <il key={index}>{w.display}</il>
-    ));
-
-  }
-}
-function tableBody(dimensions) {
-  if (dimensions.length > 0) {
-    return (dimensions.map((dlist, index) =>
-      <td key={dlist.name}>
-        <div key={index} className="dropdown">
-          <button className='btn btn-primary dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false" id={dlist.name}>{dimensions[index].title !== undefined ? dimensions[index].title : ''}</button >
-          <ul className="dropdown-menu">
-            <li key={index}><button className='dropdown-item' type="button" onClick={() => onTargetSelect("", null, index)}>&nbsp;</button></li>
-            {dlist.dimensions.map((dvalue, index) =>
-              <li key={index}><button className='dropdown-item' type="button" onClick={() => onTargetSelect(dvalue.name, dvalue.id, index)}>{dvalue.name}</button></li>
-            )}
-          </ul>
-        </div>
-      </td>)
-    );
-  }
-}
-const showAnalysisRun = (analysisRun, factCount, setAnalysisRun, setDimensions, setFactCount) => {
-  if (analysisRun != null) {
-    return (
-      <tr><td>{analysisRun.populating ? <button className="btn btn-primary" onClick={() => handleRefreshClick(setDimensions, setAnalysisRun, setFactCount)}>Refresh</button> : <button className='btn btn-primary' onClick={() => handlePopulateClick(setAnalysisRun)}>Populate</button>}</td><td>{factCount}</td><td>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(analysisRun.lastRunDateTime)}</td><td>{String(analysisRun.populating)}</td></tr>
-    );
-  } else {
-    return (
-      <tr><td><button className="btn btn-primary" onClick={() => handlePopulateClick(setAnalysisRun)}>Populate</button></td><td>No Count</td><td>Not Run</td><td>FALSE</td></tr>
-    );
-  }
-}
 function olapResultDisplay(olapResult) {
   if (olapResult != null) {
     return (
@@ -178,7 +181,6 @@ function olapResultDisplay(olapResult) {
     );
   }
 }
-
 
 export default function Analysis() {
   return <AnalysisPage />
